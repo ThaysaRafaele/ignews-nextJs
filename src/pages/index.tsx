55c09665c7e01903bfe063c9567c523446a010a8
Thaysa Lacerda <t_thaysa.lacerda@hapvida.com.br>
2023-04-26 21:07:36 -0400

    componente SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 
 import styles from './home.module.scss'
+import { SubscribeButton } from '../components/SubscribeButton'
 
 export default function Home() {
   return (
@@ -17,6 +18,7 @@ export default function Home() {
             Get access to all the publications <br />
             <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="girl coding" />
